cf48c25edbd45d223c4dcea9477de4f76dd515c3
botirk38 <botir.khaltaev@squaredup.com>
2024-12-10 18:25:16 +0000

    Feat: Add support for describe partitions API

--- src/include/kafka_response.hpp
+++ src/include/kafka_response.hpp
@@ -6,7 +6,18 @@ struct ApiVersionResponse {
   static constexpr int16_t MIN_VERSION = 0;
   static constexpr int16_t MAX_VERSION = 4;
   static constexpr int16_t UNSUPPORTED_VERSION = 35;
+  static constexpr int16_t DESCRIBE_TOPIC_PARTITIONS_KEY = 75;
 
   static void write(char *buffer, int &offset, int32_t correlation_id,
                     int16_t api_version);
+
+private:
+  static void writeMessageSizePlaceholder(char *buffer, int &offset);
+  static void writeCorrelationId(char *buffer, int &offset,
+                                 int32_t correlation_id);
+  static void writeErrorCode(char *buffer, int &offset, int16_t api_version);
+  static void writeApiVersionsEntry(char *buffer, int &offset);
+  static void writeTagBuffersAndThrottleTime(char *buffer, int &offset);
+  static void updateMessageSize(char *buffer, int final_offset);
+  static void writeDescribeTopicPartitionsEntry(char *buffer, int &offset);
 };
--- src/kafka_response.cpp
+++ src/kafka_response.cpp
@@ -4,14 +4,29 @@
 
 void ApiVersionResponse::write(char *buffer, int &offset,
                                int32_t correlation_id, int16_t api_version) {
-  // Reserve space for message size
-  offset += 4;
+  writeMessageSizePlaceholder(buffer, offset);
+  writeCorrelationId(buffer, offset, correlation_id);
+  writeErrorCode(buffer, offset, api_version);
+  writeApiVersionsEntry(buffer, offset);
+  writeDescribeTopicPartitionsEntry(buffer, offset);
+  writeTagBuffersAndThrottleTime(buffer, offset);
+  updateMessageSize(buffer, offset);
+}
 
-  // Correlation ID
+void ApiVersionResponse::writeMessageSizePlaceholder(char *buffer,
+                                                     int &offset) {
+  // Reserve space for message size that will be filled later
+  offset += 4;
+}
+
+void ApiVersionResponse::writeCorrelationId(char *buffer, int &offset,
+                                            int32_t correlation_id) {
   memcpy(buffer + offset, &correlation_id, 4);
   offset += 4;
+}
 
-  // Error code
+void ApiVersionResponse::writeErrorCode(char *buffer, int &offset,
+                                        int16_t api_version) {
   int16_t error_code =
       (api_version >= MIN_VERSION && api_version <= MAX_VERSION)
           ? 0
@@ -20,17 +35,20 @@ void ApiVersionResponse::write(char *buffer, int &offset,
   memcpy(buffer + offset, &error_code, 2);
   offset += 2;
 
-  // Number of entries
-  uint8_t num_entries = 2;
+  // Write number of entries
+  uint8_t num_entries = 3;
   memcpy(buffer + offset, &num_entries, 1);
   offset += 1;
+}
 
-  // API Versions entry
+void ApiVersionResponse::writeApiVersionsEntry(char *buffer, int &offset) {
+  // Write API key
   int16_t api_key = htons(API_VERSIONS_KEY);
+  uint8_t tag_buffer = 0;
   memcpy(buffer + offset, &api_key, 2);
   offset += 2;
 
-  // Version range
+  // Write version range
   int16_t min_ver = htons(MIN_VERSION);
   int16_t max_ver = htons(MAX_VERSION);
   memcpy(buffer + offset, &min_ver, 2);
@@ -38,12 +56,14 @@ void ApiVersionResponse::write(char *buffer, int &offset,
   memcpy(buffer + offset, &max_ver, 2);
   offset += 2;
 
-  // Tag buffers and throttle time
-  uint8_t tag_buffer = 0;
-  int32_t throttle_time = 0;
-
   memcpy(buffer + offset, &tag_buffer, 1);
   offset += 1;
+}
+
+void ApiVersionResponse::writeTagBuffersAndThrottleTime(char *buffer,
+                                                        int &offset) {
+  uint8_t tag_buffer = 0;
+  int32_t throttle_time = 0;
 
   throttle_time = htonl(throttle_time);
   memcpy(buffer + offset, &throttle_time, 4);
@@ -51,8 +71,28 @@ void ApiVersionResponse::write(char *buffer, int &offset,
 
   memcpy(buffer + offset, &tag_buffer, 1);
   offset += 1;
+}
 
-  // Set message size
-  int32_t message_size = htonl(offset - 4);
+void ApiVersionResponse::updateMessageSize(char *buffer, int final_offset) {
+  // Calculate and write the total message size at the beginning of the buffer
+  int32_t message_size = htonl(final_offset - 4);
   memcpy(buffer, &message_size, 4);
+}
+
+void ApiVersionResponse::writeDescribeTopicPartitionsEntry(char *buffer,
+                                                           int &offset) {
+  // Write API key
+  int16_t api_key = htons(DESCRIBE_TOPIC_PARTITIONS_KEY);
+  uint8_t tag_buffer = 0;
+  memcpy(buffer + offset, &api_key, 2);
+  offset += 2;
+
+  // Write version range (both min and max are 0 for DescribeTopicPartitions)
+  int16_t version = htons(0);
+  memcpy(buffer + offset, &version, 2); // MinVersion = 0
+  offset += 2;
+  memcpy(buffer + offset, &version, 2); // MaxVersion = 0
+  offset += 2;
+  memcpy(buffer + offset, &tag_buffer, 1);
+  offset += 1;
 }
